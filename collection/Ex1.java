926862499e506366c5f0f85af24f817c90fcd3fb
Raphaela Oliveira <rphaelaoliveira@gmail.com>
2022-03-10 19:31:37 -0300

    Exercício

---
+++ collection/Ex1.java
@@ -0,0 +1,28 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		String prod1 = "Refrigerante";
+		String prod2 = "Cervejas";
+		String prod3 = "Sucos";
+
+		Collection<String> estoque = new ArrayList <>();
+
+		estoque.add(prod1);
+		estoque.add(prod2);
+		estoque.add(prod3);
+		
+		System.out.println("Produtos:\n"+estoque);
+		
+		estoque.remove(prod2);
+		
+		System.out.println("\nProdutos:");
+		for (String atualizacao : estoque) {
+			System.out.println(atualizacao); }
+	}
+}
